7cc265d78e4d3b0b0f047b1feae5109fd88e0817
sshingh67 <ssuppal67@outlook.com>
2018-05-01 18:25:01 -0700

    hooked up the database

--- app.js
+++ app.js
@@ -2,17 +2,32 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const bodyParser = require('body-parser');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const db = require('./models');
+
+
 
 var app = express();
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+db.sequelize.sync().then(()=> {
+
+    console.log('checking database connection');
+})
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
+
+
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
