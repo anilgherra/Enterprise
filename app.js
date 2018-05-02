446b6b4d0a91932f6c5bc21006dc3ea793837ea5
Anil Gherra <anilgherra@yahoo.com>
2018-05-01 18:53:47 -0700

    added views engine

--- app.js
+++ app.js
@@ -14,6 +14,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
